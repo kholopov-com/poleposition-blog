14bb28a82ae07011194a9cdac75a6309c3b9bb7d
kholopov <holopov@kontur>
2025-06-16 13:02:21 +0500

    Fix css link for http(s)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <script>
     document.addEventListener('DOMContentLoaded', function () {
